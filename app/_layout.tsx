e137e2ed0f4bf025819ee552c9dc5597da2351b4
Miles <wangle2071@.com>
2025-06-17 16:17:00 +0800

    v202506171616

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,4 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
@@ -9,14 +8,14 @@ import '../app/global.css';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+  // const [loaded] = useFonts({
+  //   SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  // });
 
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
+  // if (!loaded) {
+  //   // Async font loading only occurs in development.
+  //   return null;
+  // }
 
   return (
     <ThemeProvider value={colorScheme === 'dark'? DarkTheme : DefaultTheme}>
--- app/home.tsx
+++ app/home.tsx
@@ -1,6 +1,8 @@
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+
+import { Button, Image, Text, TextInput, View } from 'react-native';
 
 type RootStackParamList = {
   '(tabs)': undefined;
@@ -10,10 +12,35 @@ export default function Home() {
   const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
 
   return (
-    <View className=" bg-blue-500 items-center justify-center">
-      <Text className="text-white text-xl">
-        Hello World
-      </Text>
-    </View>
-  );
+    <>
+      <LinearGradient
+        colors={['#3d7cff', '#2bbaff']}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 0 }}
+        className='h-[200px] flex justify-center items-center'
+      >
+        <Image 
+          source={require('../assets/images/logo.png')} 
+          className='w-[120px] h-[120px]'
+        />
+      </LinearGradient>
+      <View className='flex-1 bg-white rounded-t-[24px] -mt-8'>
+        <Text className='text-[24px] font-bold ml-6 mt-6 mr-6'>欢迎使用Yunsoo</Text>
+        <TextInput
+          placeholder='请输入邮箱'
+          className='border border-gray-300 rounded-md p-2 m-6 block'
+          focusable
+        />
+        <TextInput
+          placeholder='请输入密码'
+          className='border border-gray-300 rounded-md p-2 m-6 block'
+          focusable
+        />
+        <Button
+          title='登录'
+          color="#3b82f6"
+        />
+      </View>
+    </>
+  )
 }
